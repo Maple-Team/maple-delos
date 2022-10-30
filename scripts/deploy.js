12f0154b1e7a67308d218b11ce288726b99d765e
Liutsing <liutsingluo@163.com>
2022-10-30 11:54:33 +0800

    feat: update build script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -13,27 +13,27 @@ dockerProcss.stderr.on('data', function (data) {
   console.error(data)
 })
 
-dockerProcss.stdout.on('end', function () {
-  try {
-    execSync(
-      `docker ps -q --filter "name=maple-delos" | grep -q . && docker stop maple-delos && docker rm -fv maple-delos`
-    )
-  } catch (error) {
-    // 无docker运行会报错
-  }
-  // 可以不用暴露出来
-  exec(
-    `docker run -itd -p 3001:3000 --name maple-delos --link maple-mysql --link maple-mongodb --net maple-network  liutsing/delos:latest`,
-    (e, stdout, stderr) => {
-      if (e) {
-        throw e
-      } else {
-        if (stderr) {
-          console.error(stderr)
-        } else {
-          console.log('docker run success!')
-        }
-      }
-    }
-  )
-})
+// dockerProcss.stdout.on('end', function () {
+//   try {
+//     execSync(
+//       `docker ps -q --filter "name=maple-delos" | grep -q . && docker stop maple-delos && docker rm -fv maple-delos`
+//     )
+//   } catch (error) {
+//     // 无docker运行会报错
+//   }
+//   // 可以不用暴露出来
+//   exec(
+//     `docker run -itd -p 3001:3000 --name maple-delos --link maple-mysql --link maple-mongodb --net maple-network  liutsing/delos:latest`,
+//     (e, stdout, stderr) => {
+//       if (e) {
+//         throw e
+//       } else {
+//         if (stderr) {
+//           console.error(stderr)
+//         } else {
+//           console.log('docker run success!')
+//         }
+//       }
+//     }
+//   )
+// })
