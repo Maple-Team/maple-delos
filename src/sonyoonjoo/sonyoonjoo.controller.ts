9fe83805da50a2e68b7f5bd8268a899ed03a493f
Liutsing <liutsingluo@163.com>
2022-12-29 13:04:38 +0800

    feat: add api for syj

--- src/sonyoonjoo/sonyoonjoo.controller.ts
+++ src/sonyoonjoo/sonyoonjoo.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Param, Query, UseInterceptors } from '@nestjs/common'
+import { TransformInterceptor } from 'src/interceptor/transform.interceptor'
 import { SonyoonjooService } from './sonyoonjoo.service'
 
 @Controller('sonyoonjoo')
+@UseInterceptors(TransformInterceptor)
 export class SonyoonjooController {
   constructor(private readonly sonyoonjooService: SonyoonjooService) {}
 
   @Get()
-  findAll() {
-    return this.sonyoonjooService.findAll()
+  findWithPagination(@Query() query: { page: number; pageSize: number; year?: number; name?: string }) {
+    const { page = 1, pageSize = 10, ...rest } = query
+    return this.sonyoonjooService.findWithPagination(+page, +pageSize, { ...rest })
+  }
+
+  @Get('/category/years')
+  years() {
+    return this.sonyoonjooService.fetchYears()
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    return await this.sonyoonjooService.findById(id)
+  }
+
+  @Get('nav/:id')
+  async findPrevAndNext(@Param('id') id: string) {
+    return await this.sonyoonjooService.findPrevAndNext(id)
   }
 }
--- src/sonyoonjoo/sonyoonjoo.service.ts
+++ src/sonyoonjoo/sonyoonjoo.service.ts
@@ -7,7 +7,58 @@ import { SonYoonJooDocument, SonYoonJoo } from './schemas/sonyoonjoo.schema'
 export class SonyoonjooService {
   constructor(@InjectModel(SonYoonJoo.name) private model: Model<SonYoonJooDocument>) {}
 
-  async findAll(): Promise<SonYoonJoo[]> {
-    return this.model.find({}).skip(0).limit(10).exec()
+  async findWithPagination(
+    page: number,
+    pageSize: number,
+    rest: {
+      year?: number
+      path?: string
+    }
+  ): Promise<BaseList<SonYoonJoo>> {
+    const filterKeys: {
+      year?: number
+      path?: any
+    } = {}
+    if (rest.year) {
+      filterKeys.year = rest.year
+    }
+    if (rest.path) {
+      filterKeys.path = {
+        $regex: new RegExp(rest.path),
+      }
+    }
+    const total = await this.model.find({ ...filterKeys }).count()
+
+    const data = await this.model
+      .find({ ...filterKeys })
+      .skip((page - 1) * pageSize)
+      .limit(pageSize)
+      .sort({ date: -1 })
+      .exec()
+
+    return {
+      pagination: {
+        total,
+        current: +page,
+        pageSize,
+      },
+      records: data,
+    }
+  }
+
+  async fetchYears() {
+    return await this.model.find({}).select('year _id').distinct('year')
+  }
+
+  async findById(id: string) {
+    return await this.model.findById(id)
+  }
+
+  async findPrevAndNext(id: string) {
+    const model = await this.model.findById(id)
+    const date = model?.date
+    const prev = await this.model.findOne({ date: { $lt: date } }).sort({ date: 'desc' })
+    const next = await this.model.findOne({ date: { $gt: date } }).sort({ date: 'asc' })
+    return { prev, next }
   }
 }
