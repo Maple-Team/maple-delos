81b2ea8c169271df899f290e1fdefe0da0fd10e3
Liutsing <liutsingluo@163.com>
2023-03-18 14:50:32 +0800

    feat: 处理为模板

--- packages/delos-calc/src/app.controller.ts
+++ packages/delos-calc/src/app.controller.ts
@@ -3,12 +3,17 @@ import { AppService } from './app.service';
 import { MessagePattern } from '@nestjs/microservices';
 @Controller()
 export class AppController {
-  constructor() {
-    //
+  constructor(private service: AppService) {
+    console.log(service);
   }
 
   @MessagePattern('sum')
   sum(numArray: number[]): number {
     return numArray.reduce((p, c) => p + c, 0);
+  }
+
+  @Get('/')
+  getHello() {
+    return this.service.getHello();
   }
 }
--- packages/delos-calc/src/main.ts
+++ packages/delos-calc/src/main.ts
@@ -1,15 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
+  // http server
+  // const app = await NestFactory.create(AppModule, { cors: true });
+  // await app.listen(3000);
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.TCP,
       options: {
         host: '0.0.0.0',
-        port: 8888,
+        port: 8800, // port
       },
     },
   );
