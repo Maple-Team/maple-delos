55dcadd4cf9d772f9cb94181b0e6f76eb398ef27
Liutsing <liutsingluo@163.com>
2023-03-19 17:24:07 +0800

    fix: template content

--- packages/mqtt/src/app.controller.ts
+++ packages/mqtt/src/app.controller.ts
@@ -2,13 +2,6 @@ import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 import { EventPattern } from '@nestjs/microservices';
 
-/**
- * TODO
- * 1. log test
- * 2. user request log
- * 3. business analysis
- * 4. form body log
- */
 @Controller()
 export class AppController {
   constructor(private service: AppService) {
--- packages/mqtt/src/main.ts
+++ packages/mqtt/src/main.ts
@@ -2,14 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.TCP,
+      transport: Transport.MQTT,
       options: {
-        host: '0.0.0.0',
-        port: 8801,
+        url: isProd ? 'mqtt://mqtt-server:1883' : 'mqtt://localhost:1883',
       },
     },
   );
