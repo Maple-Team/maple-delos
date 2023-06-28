8fe9c3cba1a59193247fbc6a407b8532a4c844e7
Liutsing Robert <liutsingluo@gmail.com>
2023-06-28 17:28:21 +0800

    feat: 添加会议室服务端逻辑

--- packages/main/src/events/events.gateway.ts
+++ packages/main/src/events/events.gateway.ts
@@ -113,24 +113,21 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect, 
   afterInit() {}
 
   @WebSocketServer()
-  server: Server
+  server: Server<ServerToClientEvents>
 
+  // 测试功能
   ids: string[]
-
   @SubscribeMessage('register')
-  register(@MessageBody() id: string, @ConnectedSocket() client: Socket) {
+  register(@ConnectedSocket() client: Socket) {
+    const id = client.id
     this.ids.push(id)
-
-    console.log('id', id, client.id)
     return from([1, 2]).pipe(
       map((num) => ({
         event: num === 1 ? 'onRegister' : id,
         data:
           num === 1
-            ? { msg: 'register success', ids: this.ids }
-            : {
-                msg: `welcome ${id}`,
-              },
+            ? JSON.stringify({ msg: 'register success', ids: this.ids })
+            : JSON.stringify({ msg: `welcome ${id}` }),
       }))
     )
   }
@@ -159,5 +156,75 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect, 
   @SubscribeMessage('identity')
   async identity(@MessageBody() data: number): Promise<number> {
     return data
+  }
+
+  // 会议室业务
+  rootAdmin: string | undefined
+
+  @SubscribeMessage('create or join')
+  async createOrJoin(
+    @MessageBody() room: string,
+    @ConnectedSocket() client: Socket<ClientToServerEvents, ServerToClientEvents>
+  ) {
+    const clientsRoom = this.server.sockets.adapter.rooms.get(room)
+    const numberClients = clientsRoom?.size || 0
+
+    client.join(room)
+
+    if (numberClients === 0) {
+      // 处理创建房间后的逻辑
+      this.rootAdmin = client.id
+      client.emit('created', room, client.id)
+    } else {
+      // 处理加入房间后的逻辑，事件推送
+      this.server.sockets.in(room).emit('join', room) // 通知房间内的其他用户
+      this.server.to(client.id).emit('joined', room, client.id) // 通知客户端它加入了房间
+      this.server.sockets.in(room).emit('ready', client.id) // 房间准备创建连接
+    }
+  }
+
+  @SubscribeMessage('message')
+  async onMessage(
+    @MessageBody() data: [Message, string | null | undefined, string | null | undefined],
+    @ConnectedSocket() client: Socket<ClientToServerEvents, ServerToClientEvents>
+  ) {
+    const [message, toId, room] = data
+    if (toId) {
+      this.server.to(toId).emit('message', message, client.id) // 向特定用户发送
+    } else if (room) {
+      client.broadcast.to(room).emit('message', message, client.id) // 客户端在房间内广播数据
+    } else {
+      client.broadcast.emit('message', message, client.id) // 客户端广播消息
+    }
+  }
+
+  @SubscribeMessage('kickout')
+  async onKickout(
+    @MessageBody() data: [string, string],
+    @ConnectedSocket() client: Socket<ClientToServerEvents, ServerToClientEvents>
+  ) {
+    const [socketId, room] = data
+    if (this.rootAdmin === client.id) {
+      client.broadcast.emit('kickout', socketId)
+      this.server.sockets.sockets.get(socketId)?.leave(room)
+    }
+  }
+
+  @SubscribeMessage('leave room')
+  async onLeaveRoom(
+    @MessageBody() room: string,
+    @ConnectedSocket() client: Socket<ClientToServerEvents, ServerToClientEvents>
+  ) {
+    client.leave(room)
+    client.emit('left room', client.id)
+    client.broadcast.to(room).emit('message', { type: 'leave' }, client.id)
+  }
+
+  @SubscribeMessage('disconnecting')
+  async onDisconnecting(@ConnectedSocket() client: Socket<ClientToServerEvents, ServerToClientEvents>) {
+    client.rooms.forEach((room) => {
+      if (room === client.id) return
+      client.broadcast.to(room).emit('message', { type: 'leave' }, client.id)
+    })
   }
 }
