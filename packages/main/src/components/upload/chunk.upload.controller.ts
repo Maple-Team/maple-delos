14bbd082717724b6fb8bd3072d6f366ff2cbfc15
Liutsing <liutsingluo@163.com>
2023-07-11 00:32:10 +0800

    feat: ~

--- packages/main/src/components/upload/chunk.upload.controller.ts
+++ packages/main/src/components/upload/chunk.upload.controller.ts
@@ -1,39 +1,50 @@
 import os from 'node:os'
 import path from 'node:path'
-import { statSync } from 'node:fs'
-import { createHash } from 'node:crypto'
+import { readdirSync, statSync, writeFile } from 'node:fs'
 import { Body, Controller, Get, Post, Query, UploadedFiles, UseInterceptors } from '@nestjs/common'
-import { FilesInterceptor } from '@nestjs/platform-express'
 import { TransformInterceptor } from 'src/interceptor/transform.interceptor'
 import { mkdirSafeSync } from '@liutsing/node-utils'
-import { diskStorage } from 'multer'
+import { AnyFilesInterceptor } from '@nestjs/platform-express'
 
 // https://javascript.plainenglish.io/implement-concurrent-upload-of-large-files-in-javascript-53519a0d2eee
 
 @Controller('chunk-upload')
 @UseInterceptors(TransformInterceptor)
 export class ChunkUploadController {
-  @Post()
+  @Post('single')
   @UseInterceptors(
-    FilesInterceptor('chunk-file', 10, {
-      storage: diskStorage({
-        destination: (req, file, callback) => {
-          //   const directory = createHash('md5').update(file.buffer).digest('hex')
-          const directory = ''
-          const fileDest = path.resolve(os.tmpdir(), `./uploads/${directory}`)
-          console.log(file)
-          mkdirSafeSync(fileDest, { recursive: true })
-          callback(null, fileDest)
-        },
-      }),
-    })
+    // FilesInterceptor('chunk-file', 1, {
+    //   storage: multer.diskStorage({
+    //     destination: (req, file, callback) => {
+    //       // Access other non-binary data from req.body
+    //       // @ts-expect-error: xx
+    //       console.log(req.body, req.a, '================')
+    //       const directory = ''
+    //       const fileDest = path.resolve(os.tmpdir(), `./uploads/${directory}`)
+    //       mkdirSafeSync(fileDest, { recursive: true })
+    //       console.log(fileDest)
+    //       callback(null, fileDest)
+    //     },
+    //   }),
+    // })
+    AnyFilesInterceptor()
   )
   upload(@Body() body, @UploadedFiles() files: Array<Express.Multer.File>) {
-    // console.log(files[0])
-    return files[0].path
+    const { fileName, fileMd5 } = body
+    const file = files[0]
+    const fileDest = path.resolve(os.tmpdir(), `./uploads/${fileMd5}`)
+    // const fileDest = path.resolve('/tmp', 'uploads', directory);
+
+    mkdirSafeSync(fileDest, { recursive: true })
+
+    writeFile(`${fileDest}/${file.originalname}`, file.buffer, (e) => {
+      if (e) throw e
+      else console.log('write')
+    })
+    return fileDest
   }
 
-  @Get()
+  @Get('exists')
   fileExist(@Query() query) {
     const { name: fileName, md5: fileMd5 } = query as { name: string; md5: string }
     const imageDir = path.resolve(process.cwd(), './uploads/images')
@@ -45,7 +56,26 @@ export class ChunkUploadController {
         isExists: true,
       }
     } else {
-      const chunksIds = []
+      const fileDest = path.resolve(os.tmpdir(), `./uploads/${fileMd5}`)
+      if (statSync(fileDest).isDirectory()) {
+        return {
+          isExists: false,
+          chunkIds: readdirSync(fileDest),
+        }
+      }
     }
+  }
+
+  @Get('concat-files')
+  async concatFiles(@Query() query) {
+    const { name: fileName, md5: fileMd5 } = query
+    // await concatFiles(path.join(TMP_DIR, fileMd5), path.join(UPLOAD_DIR, fileName))
+  }
+
+  /**
+   * 合并chunks
+   */
+  async _concatFiles(sourceDir: string, targetPath: string) {
+    console.log(sourceDir, targetPath)
   }
 }
