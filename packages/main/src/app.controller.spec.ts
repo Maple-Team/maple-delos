e69ab3e167c9524217b74decb2d43555f76a7ab6
Liutsing Robert <liutsingluo@gmail.com>
2024-11-22 11:58:31 +0800

    feat: 更新jest配置相关

--- packages/main/src/app.controller.spec.ts
+++ packages/main/src/app.controller.spec.ts
@@ -1,15 +1,19 @@
 import type { TestingModule } from '@nestjs/testing'
 import { Test } from '@nestjs/testing'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { User } from '@/components/users/entities/user.entity'
 
 describe('AppController', () => {
   let appController: AppController
 
+  // FIXME 测试
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
+      imports: [TypeOrmModule.forFeature([User])],
     }).compile()
 
     appController = app.get<AppController>(AppController)
@@ -17,7 +21,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!')
+      expect(appController.hello()).toBe('Hello World!')
     })
   })
 })
