c452a547b86a94938af169b5da8d743528e77585
Liutsing Robert <liutsingluo@gmail.com>
2025-02-25 21:57:04 +0800

    feat: 添加一个公共的types库

---
+++ packages/types-utils/.eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: '@liutsing/eslint-config',
+  rules: {
+    '@typescript-eslint/require-await': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    'prettier/prettier': 'off',
+    'vue/component-name-in-template-casing': 'off',
+    'multiline-ternary': 'off',
+    'operator-linebreak': 'off',
+  },
+}
