70772ba25d18afa7ca146eb62cba5b64d12bb628
Liutsing Robert <liutsingluo@gmail.com>
2025-03-10 11:27:47 +0800

    feat: 更新响应体的status

--- packages/main/src/interceptor/transform.interceptor.ts
+++ packages/main/src/interceptor/transform.interceptor.ts
@@ -1,8 +1,7 @@
 import type { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common'
-import { Injectable } from '@nestjs/common'
+import { HttpStatus, Injectable } from '@nestjs/common'
 import { map } from 'rxjs/operators'
 import type { BaseResponse } from '@liutsing/types-utils'
-import { StatusEnum } from '@/enum/status'
 
 @Injectable()
 export class TransformInterceptor<T> implements NestInterceptor<T, BaseResponse<T>> {
@@ -18,7 +17,7 @@ export class TransformInterceptor<T> implements NestInterceptor<T, BaseResponse<
     return next.handle().pipe(
       map((data) => {
         return {
-          status: StatusEnum.OK,
+          status: HttpStatus.OK,
           message: 'success',
           data,
           timestamp: new Date().getTime(),
