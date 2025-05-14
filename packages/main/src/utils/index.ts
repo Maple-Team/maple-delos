cd27bb64452ff369b0d28c22b96a3e4614bdd867
Liutsing Robert <liutsingluo@gmail.com>
2025-05-14 22:14:15 +0800

    feat(utils): 添加formatName函数用于格式化名称
    新增formatName函数，用于处理字符串中的特殊字符和空格，将其替换为下划线或移除。特别处理了"▼SonYoonJoo▲"字符串，确保其被移除。该函数旨在提高名称处理的统一性和可读性。

--- packages/main/src/utils/index.ts
+++ packages/main/src/utils/index.ts
@@ -1,3 +1,13 @@
 import dayjs from 'dayjs'
 
 export const getTimeStr = () => dayjs().format('YYYY-MM-DD HH:mm:ss SSS')
+export const formatName = (name: string) => {
+  return `${name
+    .replace(/\s*\(/g, '_')
+    .replace(/\s*（/g, '_')
+    .replace(/\)/g, '')
+    .replace(/）/g, '')
+    .replace(/"/g, '')
+    .replace(/&/g, '')
+    .replace('▼SonYoonJoo▲', '')}`
+}
