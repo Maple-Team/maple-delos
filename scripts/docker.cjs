b903b5e1a717b7dbc8e2b6f235d8194b69d0a05a
Liutsing Robert <liutsingluo@gmail.com>
2025-05-14 23:20:21 +0800

    ci: update docker-compose and docker script to use environment variables
    Modify docker-compose.yml to use environment variables for MINIO_ACCESS_KEY and MINIO_SECRET_KEY. Update docker.cjs to generate .env file with platform-specific MinIO credentials. This change improves security and flexibility by avoiding hardcoding sensitive information.

--- scripts/docker.cjs
+++ scripts/docker.cjs
@@ -2,14 +2,29 @@ const path = require('node:path')
 const root = process.cwd()
 const { execSync, exec } = require('node:child_process')
 const { writeFile } = require('node:fs')
+const os = require('node:os')
 
 const { version } = require(path.resolve(root, './package.json'))
-
-writeFile(path.resolve(root, './.env'), `VERSION=${version}`, (err) => {
-  if (err) {
-    console.error(err)
-  }
-})
+const isWin = os.platform() === 'win32'
+writeFile(path.resolve(root, './.env'),
+  isWin ?
+    // windows minio server
+    `
+VERSION=${version}
+MINIO_ACCESS_KEY=e68Cu2Fybl0KKJEs5i67
+MINIO_SECRET_KEY=ztHk5Z4tGLnYaiobFfD5cVfDN5R7pQtmyvc2MaUs
+`:
+    // mac  minio server
+    `
+VERSION=${version}
+MINIO_ACCESS_KEY=363wbyRsqOAsWXt7jsWB
+MINIO_SECRET_KEY=sUjdiu0xgCiGwwdFCsrG4YDR1u1xepC7KxjQqgZg
+`
+  , (err) => {
+    if (err) {
+      console.error(err)
+    }
+  })
 const APP_VERSION = execSync('git rev-parse HEAD').toString().trim().slice(0, 8)
 // 构建 docker build . --target xx --tag xxx
 // 不使用缓存构建 docker build --no-cache . --target xx --tag xxx
